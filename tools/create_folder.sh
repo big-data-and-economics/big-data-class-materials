67692c306228d828a8a3ea25ea4ebacb0f3db1bd
Kyle Coombs <kyle.g.coombs@gmail.com>
2023-10-11 12:49:23 -0400

    create_folder works!

--- tools/create_folder.sh
+++ tools/create_folder.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Check if folder name and subtitle are specified
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 folder_name subtitle"
+# Takes three arguments, the folder name, subtitle, and type
+
+# Check if folder name, subtitle, and type are specified
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 folder_name subtitle type"
+    exit 1
+fi
+
+# type can only be lecture or slides
+if [ "$3" != "lecture" ] && [ "$3" != "slides" ]; then
+    echo "Type can only be lecture or slides"
     exit 1
 fi
 
@@ -11,6 +19,12 @@ mkdir -p "$1/tex"
 mkdir -p "$1/css"
 
 # Create .Rmd file with preamble
+
+if [ "$3" == "lecture" ]; then
+
+cp 10-regression/css/*.css "$1/css"
+cp 10-regression/tex/*.tex "$1/tex"
+
 echo "---
 title: "Big Data and Economics"
 subtitle: "Regression analysis in R"
@@ -57,3 +71,59 @@ knitr::opts_chunk$set(echo = TRUE, cache = TRUE, dpi=300)
 
 
 " > "$1/$1.Rmd"
+elif [ "$3" == "slides" ]; then
+echo "---
+---
+title: "Big Data and Economics"
+subtitle: "$2"
+author: "Kyle Coombs"
+date: \"Bates College | [ECON/DCS 368](https://github.com/ECON368-fall2023-big-data-and-economics)\"
+output:
+  xaringan::moon_reader:
+    css: [default, metropolis, metropolis-fonts] 
+    lib_dir: libs
+    nature:
+      highlightStyle: github
+      highlightLines: true
+      highlightSpans: true
+      countIncrementalSlides: false
+---
+name: toc
+
+\`\`\`{css, echo=FALSE}
+@media print {
+  .has-continuation {
+    display: block !important;
+  }
+}
+\`\`\`
+
+\`\`\`{r setup, include=FALSE}
+options(htmltools.dir.version = FALSE)
+library(knitr)
+opts_chunk$set(
+  fig.align="center", fig.width=6, fig.height=4, 
+  # out.width="748px", #out.length="520.75px",
+  dpi=300, #fig.path='Figs/',
+  cache=T#, echo=F, warning=F, message=F
+  )
+
+library(tidyverse)
+\`\`\`
+
+# Table of contents
+
+---
+class: inverse, center, middle
+
+# Next lecture: 
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=796px></html>
+
+\`\`\`{r gen_pdf, include = FALSE, cache = FALSE, eval = TRUE}
+infile=knitr::current_input() %>% str_replace(".Rmd", ".html")
+#infile = list.files(pattern = '.html')
+pagedown::chrome_print(input = infile, timeout = 100)
+\`\`\`
+
+" > "$1/$1.Rmd"
+fi
