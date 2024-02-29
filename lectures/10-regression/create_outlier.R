c2dd419eb0510364de4d3908ad920153c16aa80e
Kyle Coombs <kyle.g.coombs@gmail.com>
2024-02-28 21:28:58 -0500

    new subset

--- lectures/10-regression/create_outlier.R
+++ lectures/10-regression/create_outlier.R
@@ -18,9 +18,10 @@ select(
 )
 
 opp_atlas <- opp_atlas %>% left_join(covariates) %>%
-  left_join(fips) %>%
-  mutate(
-    poor_share1990=ifelse(is.na(poor_share1990),99,poor_share1990),
-    kfr_pooled_pooled_p25=ifelse(is.na(kfr_pooled_pooled_p25),99,kfr_pooled_pooled_p25)) 
+  mutate(random = runif(nrow(opp_atlas)),
+     job_growth_quartile = as.integer(cut(ann_avg_job_growth_2004_2013, breaks = quantile(ann_avg_job_growth_2004_2013, probs = c(0, 0.25, 0.5, 0.75, 1),na.rm=TRUE),label=c(1,2,3,4))),
+    poor_share1990=ifelse(is.na(poor_share1990) | random>.95,99,poor_share1990),
+    kfr_pooled_pooled_p25=ifelse(is.na(kfr_pooled_pooled_p25) | random<.05,99,kfr_pooled_pooled_p25)) %>%
+  select(-random) 
 
 write_csv(opp_atlas, "opp_atlas_amended.csv")
